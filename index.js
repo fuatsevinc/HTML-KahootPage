709d849cbff80165674fe92fffa93d955067e468
fuatsevinc <fuatsevinc@icloud.com>
2022-02-21 19:20:49 +0100

    index.js page created

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('» index.js is running');
+
+const person = {
+  firstName: 'Barry',
+  lastName: 'Mitchell',
+  birthYear: 1977,
+  skills: ['JS', 'AWS', 'Docker', 'Python'],
+  employed: true,
+};
+
+console.log(person);
